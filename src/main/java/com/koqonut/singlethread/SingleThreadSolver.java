9ca7e9036c3826912890f8f66cf4f02ab279e18d
koqonut <9313346+koqonut@users.noreply.github.com>
2024-07-06 22:22:52 -0400

    increase warmup and measurement runs

--- src/main/java/com/koqonut/singlethread/SingleThreadSolver.java
+++ src/main/java/com/koqonut/singlethread/SingleThreadSolver.java
@@ -61,8 +61,8 @@ public class SingleThreadSolver {
     }
 
     @Benchmark
-    @Warmup(iterations = 0)
-    @Measurement(iterations = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 2)
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.MICROSECONDS)
     @Fork(value = 1, warmups = 0, jvmArgsAppend = {"-Xlog:gc*:out/gc_s_16g.log:time,level,tags", "-Xms16g", "-Xmx16g", "-XX:+UseStringDeduplication"})
